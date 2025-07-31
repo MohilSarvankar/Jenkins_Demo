394ebcd26e68b2bef64a7dacd16d1713f7d744e5
Mohil Sarvankar <mohilsarvankar23@gmail.com>
2025-07-31 17:17:07 +0530

    Multiply and divide added

--- CalculatorMain.java
+++ CalculatorMain.java
@@ -5,6 +5,8 @@ public class CalculatorMain{
 	
 		System.out.println("Sum of 10 and 5 : " + c.add(10,5));
 		System.out.println("Subtraction of 10 and 5 : " + c.subtract(10,5));
+		System.out.println("Multiplication of 10 and 5 : " + c.multiply(10,5));
+		System.out.println("Division of 10 and 5 : " + c.divide(10,5));
 		System.out.println("The end");
 
 	}
