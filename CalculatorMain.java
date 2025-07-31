36568bf3f43d1c4e5b58cce236cd008a445e7a11
Mohil Sarvankar <mohilsarvankar23@gmail.com>
2025-07-31 16:07:41 +0530

    Calculatot files added

---
+++ CalculatorMain.java
@@ -0,0 +1,10 @@
+public class CalculatorMain{
+	
+	public static void main(String[] args){
+		Calculator c = new Calculator();
+	
+		System.out.println("Sum of 10 and 5 : " + c.add(10,5));
+		System.out.println("Subtraction of 10 and 5 : " + c.subtract(10,5));
+
+	}
+}
